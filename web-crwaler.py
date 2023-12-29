86ee2b096a4ecc933848d7c30dd7af48d182a4d1
southAndy <zatchbell.1206@gmail.com>
2023-12-29 17:22:45 +0800

    feat(bs4): create basic structure of bs4

--- web-crwaler.py
+++ web-crwaler.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.tfam.museum/index.aspx?ddlLang=zh-tw'
+url = 'https://montue.ntue.edu.tw/exhibitions/'
 response = requests.get(url)
 # 如果回傳值是 200 代表請求成功
 if response.status_code == 200:
-    # 並將其儲存到檔案中
-    with open('ptt-stock.html', 'w', encoding='utf-8') as file:
-        file.write(response.text)
+    # 將回傳值的文字內容取出
+    museumContent = BeautifulSoup(response.text, 'html.parser')
+    # 取出展覽文章標題
+    titles = museumContent.find_all('div', class_='text')
+    print(titles)
+    # 將資料整理成 json 格式
+
+    # with open('ptt-stock.html', 'w', encoding='utf-8') as file:
+    #     file.write(response.text)
 
 
 # 檔案名稱: ptt-stock.html
