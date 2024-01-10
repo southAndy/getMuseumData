57782bf4f350c1e447bb5b5b5542caa92aec47bf
southAndy <zatchbell.1206@gmail.com>
2024-01-10 16:40:43 +0800

    test(db): add basic operator of db

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -13,15 +13,18 @@ def get_database():
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
 
-    exhibitionDB = client["exhibitions"]
-    exhibitionCol = exhibitionDB["exhibitions"]
-    print("Connected to MongoDB Atlas", exhibitionDB)
-    print(client.list_database_names())
-    client.close()
+    # 切換到展覽資料庫
+    exhibitionDB = client["exhibition"]
+
+    # 新增一筆展覽資料
+    exhibitionDB["exhibitions"].insert_one(
+        {"title": "台北美術館大展", "description": "test"})
+
+    # 查詢資料庫內的集合
+    print(exhibitionDB.list_collection_names())
+
+    # 查詢集合內的全部資料
+    print('所有展覽', exhibitionDB["exhibitions"].find())
 
 
-# This is added so that many files can reuse the function get_database()
-# if __name__ == "__main__":
-    # Get the database
-    # dbname = get_database()
 get_database()
